a64d2ef5007482786c1aab694272d7f967242daf
Ben Dunkin <chewygumbal@gmail.com>
2020-01-02 18:54:49 -0800

    question 4, part 1

---
+++ 4/main.rs
@@ -0,0 +1,34 @@
+fn is_match(number: u32) -> bool {
+    let digits = number.to_string();
+
+    let mut previous_value =  digits.chars().nth(0).unwrap().to_digit(10).unwrap();
+    let mut has_matching_adjacent_values = false;
+
+    for digit in digits.chars().skip(1) {
+        let value: u32 = digit.to_digit(10).unwrap();
+
+        if value == previous_value {
+            has_matching_adjacent_values = true;
+        }
+
+        if value < previous_value {
+            return false;
+        }
+
+        previous_value = value;
+    }
+
+    return has_matching_adjacent_values;
+}
+
+fn main() {
+    let mut matching_passwords = 0;
+    for i in 359282..820402 {
+        if is_match(i) {
+            matching_passwords += 1;
+            println!("{} is a match!", i);
+        }
+    }
+
+    println!("{} matching passwords.", matching_passwords);
+}
